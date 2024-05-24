5eefabe8f7df4c82add6e001705a3d63a6c116fe
KyleCreek <kyle.a.creek@gmail.com>
2024-05-23 21:58:40 -0400

    Added Parlay Testing Outline

--- routes/parlay.test.js
+++ routes/parlay.test.js
@@ -15,4 +15,56 @@ describe("Testing Parlay Enpoints", () => {
     const paralay1 = {};
     const parlay2 = {};
 
+    describe("POST /parlay/", () => {
+        it("Should Reject Parlays with less than 2 Bets", async () =>{
+
+        });
+        it("Should Reject Parlays When Provided Invalid Bet Ids", async () => {
+
+        });
+        it("Should Create a Parlay With 2 or More Bets", async () => {
+
+        });
+
+    });
+    describe("GET /parlay/", () => {
+        it("Non-Admin: Should only Return Bets of Which User is Participant", async () => {
+
+        });
+        it("Admin: Should Return All Parlays", async () => {
+
+        });
+    describe("GET /parlay/:id", () => {
+        it("Non Admin: Only Owner Shall View Parlay", async () => {
+
+        });
+        it("Non Admin: User shall view their own Parlay", async () => {
+
+        });
+        it("Admin: Admin Users Shall View all Parlays", async () => {
+
+        });
+
+    });
+    describe("DELETE /parlay/:id", () => {
+        it("Non Admin: Should Not Allow User to DELETE Parlay", async () => {
+
+        });
+        it("Admin: Should Allow Admin to DELETE Parlay", async () => {
+
+        });
+
+    });
+    describe("PUT /parlay/:id", () => {
+        it("Non Admin: Should Not Allow User to EDIT Parlay", async () => {
+
+        });
+        it("Admin: Should Allow Admin to EDIT Parlay", async () => {
+
+        });
+
+    });
+
+    });
+
 });
