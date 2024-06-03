fbd06505d4b7e9420fc1e6f072ff3a066762d1ee
KyleCreek <kyle.a.creek@gmail.com>
2024-06-02 20:59:08 -0700

    Trying to deploy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 const mongoURL = process.env.MONGO_CONNECTION_STRING || 'mongodb://127.0.0.1/330-Final-KyleCreek';
 
+/*
 mongoose
   .connect("mongodb://127.0.0.1/330-Final-Creek", {})
   .then(() => {
@@ -13,4 +14,10 @@ mongoose
   })
   .catch((e) => {
     console.error(`Failed to start server:`, e);
+  });
+  */
+  mongoose.connect(mongoURL, {}).then(() => {
+    server.listen(port, '0.0.0.0', () => {
+      console.log(`Server is listening on http://localhost:${port}`);
+    });
   });
